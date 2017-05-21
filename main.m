e0eb10fc22a6f63076a31761187e97cc25e5fa22
MichelsonChan <michelsonchan.apple.hksar@gmail.com>
2017-05-22 00:12:36 +0800

    major update on core function

--- main.m
+++ main.m
@@ -2,20 +2,16 @@ close all
 clear
 clc
 
-load HSI
-RGBIMG = HSI.RGB ;
-HSI    = HSI.HSICUBE ;
-HSI = HSI / max( HSI(:) ) ;
-HSI( HSI < 0 ) = 0 ;
+% load image data
+load HSI_AVIRIS_CUPRITE    % loaded variable : "HSI"
+% load wavelength data
+load HSI_AVIRIS_wavelength % loaded variable : "wavelength"
 
-GEN_opt.type           = 'fullspectral'  ; GEN_opt.outputFileName = 'HSI' ;
-%GEN_opt.type           = 'hyperspectral' ; GEN_opt.outputFileName = 'HS'  ;
-%GEN_opt.type           = 'multispectral' ; GEN_opt.outputFileName = 'MS'  ;
-GEN_opt.outputExt      = 'png' ;
-GEN_opt.dsRatio        = 6     ;
-GEN_opt.G              = construct_G_Gaussian( size(HSI,1) , size(HSI,2) , 11 , 4.7 , GEN_opt.dsRatio ) ;
-GEN_opt.F              = construct_F_Uniform( HSI , 15 ) ;
-GEN_opt.RGBIMG         = RGBIMG ;
-GEN_opt.background     = 'white' ; % 'black' ;
-
-GEN_2DHSI( HSI , GEN_opt ) ;
+HSI          = HSI(:,:,[3:103,114:147,168:220]) ; % trancate noisy and water absorption band
+L            = 80 ;
+HSI          = HSI(:,:,1:L) ;
+wavelength   = wavelength(1:L) ;
+HSI          = ( HSI - min(HSI(:)) ) / mean( [ min(HSI(:)) , mean(HSI(:)) ] ) ;
+band_separat = 5 ;
+HSI_3D_PERSP = GEN_2DHSI( HSI , wavelength , band_separat ) ;
+figure , imshow( HSI_3D_PERSP ) ;
